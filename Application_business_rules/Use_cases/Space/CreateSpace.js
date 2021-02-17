7ec0998daecfcafcb1df61cca1056776a6d96ace
Lorena Costa <lorenacostace@gmail.com>
2021-02-17 13:02:46 +0100

    Delete coment in CreateSpace case use

--- Application_business_rules/Use_cases/Space/CreateSpace.js
+++ Application_business_rules/Use_cases/Space/CreateSpace.js
@@ -3,7 +3,7 @@ const { TYPES_ERROR } = require('../../../Enterprise_business_rules/Manage_error
 
 module.exports = async (space, { spaceRepositoryMySQL }) => {
   const name = space.getName();
-  // Se comrpueba que el aula o laboratorio no existe mediante el nombre
+  // Se comprueba que el aula o laboratorio no existe mediante el nombre
   const nameExist = await spaceRepositoryMySQL.getName(name);
   if (nameExist) {
     throw new ResponseError(TYPES_ERROR.ERROR, 'Ese aula o laboratorio ya está registrado', 'space_exist');
